d3c17d1be67ceab9ae7a4a94357b181f5dd5041d
Linkitoka <linkitok44@gmail.com>
2022-01-20 21:46:12 +0100

    Add a if for delete remove image remplaced

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -34,6 +34,13 @@ exports.modifySauce = (req, res, next) => {
       ...JSON.parse(req.body.sauce),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
+  if (req.file) {
+    Sauce.findOne({ _id: req.params.id })
+      .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => { console.log("fichier supprimé") })
+      })
+  }
   Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
     .then(res.status(200).json({ message: 'Sauce modifié !' }))
     .catch((error) => res.status(400).json({ error }));
